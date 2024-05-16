cb2d31fa6dfcf4ba4cd741d0abadc9ee0cff7344
yeeun Chang 장예은 <156410012+SelinaChang4002@users.noreply.github.com>
2024-05-16 23:09:01 +0900

    Update Table.java
    isSeatOccupiedRandom() private를 public으로 바꿨고(random값 할당하는 함수) 그리고 변수 static으로 돌렸고 그걸로 인해서 void없앰
    
    Signed-off-by: yeeun Chang 장예은 <156410012+SelinaChang4002@users.noreply.github.com>

--- OOP-teamproject/src/Table.java
+++ OOP-teamproject/src/Table.java
@@ -4,7 +4,7 @@ public class Table {
     private int[] orderList;
     private int totalPrice;
     private boolean isAvailable;
-
+    static boolean isOccupied
     public Table() {
         orderList = [0,0,0,0,0,0]; //[황태해장국 정식, 순두부 정식, 뚝배기 불고기 정식, 전북 갈비탕, 탄산음료, 술] fixed index
         totalPrice = 0;
@@ -98,34 +98,20 @@ public class Table {
   //  }
     //-------------
 
-    private boolean isSeatOccupiedRandom() {
+    public void isSeatOccupiedRandom() {
         // 랜덤한 참/거짓 값을 생성하여 시간에 따른 자리 차있는지 여부 확인
         Random random = new Random();
         randomValue = random.nextBoolean();
         isAvailable = randomValue;
         Hall.getInstance().updateSeat(0, randomValue);
-        
-        
-        return random.nextBoolean();
+
+        isOccupied = random.nextBoolean();    // static한 isOccupied이 random.nextBoolean()값 바로 받는걸로
     }
 
     private void seatOccupied(Boolean x){ //seat available 여부를 table field와 hall의 spatial list인 seatList에 수동 업데이트(예은/정우-예약에 따른 자리 업데이트, 손님 방문에 따른 자리 업데이트에 사용)
         isAvailable = x;
         Hall.getInstance().updateSeat(0, x);
     }
-
-    public void seatProtocol() {
-        boolean isOccupied = isSeatOccupiedRandom();
-
-        if (isOccupied) {
-            // 안내사항을 어떻게 할 것인지
-            System.out.println("죄송합니다. 남아있는 자리가 없습니다. 잠시만 기다려주세요.");
-        } else {
-            System.out.println("현재 남아있는 좌석이 있습니다. 안내드리겠습니다.");
-        }
-    }
-
-    // public void setAvailable(boolean available) {
-    //     isAvailable = available;
+    
     }
     
