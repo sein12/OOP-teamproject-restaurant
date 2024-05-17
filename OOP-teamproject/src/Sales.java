85589cf2581c8131106148d354f8d6a66019849a
raspberry236 <71596149+wjddn4502@users.noreply.github.com>
2024-05-17 17:01:05 +0900

    Sales.java
    Signed-off-by: raspberry236 <71596149+wjddn4502@users.noreply.github.com>

--- OOP-teamproject/src/Sales.java
+++ OOP-teamproject/src/Sales.java
@@ -2,23 +2,8 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Sales {
-  public int calculateSales(ArrayList<String> orderList) {
-    MenuCustomerPrice menu = MenuCustomerPrice.getInstance();
-    RevenueAndCost.revenue = 0;
-
-    for (String item : orderList) {
-      if (menu.getCoursePrice(item) > 0) {
-        RevenueAndCost.revenue += menu.getCoursePrice(item);
-      } else if (menu.getDrinkPrice(item) > 0) {
-        RevenueAndCost.revenue += menu.getDrinkPrice(item);
-      }
-    }
-
-    return RevenueAndCost.revenue;
-
-  }
-
-  public void printSalesReport(ArrayList<String> orderList) {
-    System.out.println("총 매출: " + RevenueAndCost.revenue + "원");
+  public static int Sales = 0;
+  public int Sales(){
+    return Sales;
   }
 }
