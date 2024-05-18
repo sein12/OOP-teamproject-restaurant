a549532b983772d013003652c635091167d284b6
sein12 <moses1008@naver.com>
2024-05-18 10:54:41 +0900

    modify ReservationSystem

--- OOP-teamproject/src/ReservationSystem.java
+++ OOP-teamproject/src/ReservationSystem.java
@@ -1,32 +1,38 @@
 import java.util.Random;
 import java.util.Scanner;
 
-import com.sun.org.apache.bcel.internal.generic.NEW;
 public class ReservationSystem{
-public static synchronized ReservationSystem getInstance() {
+    private static ReservationSystem instance; // 싱글톤 인스턴스
+    private static final boolean[] reservationTime = new boolean[7]; // 예약 시간 배열
+    
+    private ReservationSystem() { // private 생성자
+    }
+
+    public static synchronized ReservationSystem getInstance() {
         if (instance == null) {
             instance = new ReservationSystem();
         }
         return instance;
     }
-    public static boolean[] reservationtime = new boolean[7];//db에서 자체 진행할건지
+
     public void selectReservationTime() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("예약할 시간대를 선택해주세요 (12시부터 18시까지): ");
         int selectedTime = scanner.nextInt();
         if (selectedTime >= 12 && selectedTime <= 18) {
-            reservationtime[selectedTime - 12] = true;
+            reservationTime[selectedTime - 12] = true;
             System.out.println(selectedTime + "시에 예약되었습니다.");
-            Hall.getInstance().updateSeat(0, selectedTime);
+            Hall.getInstance().updateSeat(0, true); // 좌석 상태를 예약된 것으로 업데이트
         } else {
             System.out.println("잘못된 시간대입니다. 12시부터 18시까지 선택해주세요.");
         }
     }
 
-    public void Call_resevation() {
-        for (int i = 0; i < reservationtime.length; i++) {
-            if (reservationtime[i]=true) {
-                System.out.println("오늘 "+reservationtime[i]+"시에 예약이 준비되어 있습니다.");}
+    public void callReservation() {
+        for (int i = 0; i < reservationTime.length; i++) {
+            if (reservationTime[i]) {
+                System.out.println("오늘 " + (i + 12) + "시에 예약이 준비되어 있습니다.");
             }
+        }
     }
 }
