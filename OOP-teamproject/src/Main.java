91c90e74a903fb629ff7a4ebc2d02e29ad2a0fbd
sein12 <moses1008@naver.com>
2024-05-18 16:23:06 +0900

    modify main

--- OOP-teamproject/src/Main.java
+++ OOP-teamproject/src/Main.java
@@ -1,5 +1,4 @@
 public class Main {
-	
     // Essential Physical Resource in the Main!
     public static Table table1 = new Table();
     public static Preopen pre = new Preopen();
@@ -8,7 +7,6 @@ public class Main {
         System.out.println("레스토랑 프로젝트");
         System.out.println("------------");
         System.out.println("이재윤, 이정우, 장예은, 최세인");
-
         System.out.println("비우고 채우고 나누다. 비채나에 오신 것을 환영합니다.");
         System.out.println("현재는 가게 오픈 전, 준비 시간");
 
