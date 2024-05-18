b77028c0700b8cafeee317b40179f8ab2f9e3270
sein12 <moses1008@naver.com>
2024-05-18 10:49:39 +0900

    Modify Menu

--- OOP-teamproject/src/MenuCustomerPrice.java
+++ OOP-teamproject/src/MenuCustomerPrice.java
@@ -1,15 +1,15 @@
 public class MenuCustomerPrice {
     private static MenuCustomerPrice instance; // 싱글톤
 
-    static String[] courseMenu = { "황태해장국 정식", "순두부 정식", "뚝배기 불고기 정식", "전복 갈비탕" };
-    static String[] drinkMenu = { "탄산음료", "술" };
-    static int[] coursePriceInt = { 17000, 18000, 19000, 25000 }; // 가격을 정수형으로 변경
-    static int[] drinkPriceInt = { 5000, 8000 }; // 가격을 정수형으로 변경
+    private static final String[] COURSE_MENU = { "황태해장국 정식", "순두부 정식", "뚝배기 불고기 정식", "전복 갈비탕" };
+    private static final String[] DRINK_MENU = { "탄산음료", "술" };
+    private static final int[] COURSE_PRICE_INT = { 17000, 18000, 19000, 25000 }; // 가격을 정수형으로 변경
+    private static final int[] DRINK_PRICE_INT = { 5000, 8000 }; // 가격을 정수형으로 변경
 
     private MenuCustomerPrice() { // 생성자
     }
 
-    public static synchronized MenuCustomerPrice getInstance() {
+    public static MenuCustomerPrice getInstance() {
         if (instance == null) {
             instance = new MenuCustomerPrice();
         }
@@ -17,56 +17,44 @@ public class MenuCustomerPrice {
     }
 
     public String[] getCourseMenuList() { // 메뉴의 리스트를 반환함
-        return courseMenu;
+        return COURSE_MENU;
     }
-
+    
     public int getCoursePrice(String menu) { // 메뉴 이름을 parameter로 받아 해당 메뉴의 가격을 int 형식으로 반환.
-        int returnValue = 0;
         switch (menu) {
             case "황태해장국 정식":
-                returnValue = coursePriceInt[0];
-                break;
+                return COURSE_PRICE_INT[0];
             case "순두부 정식":
-                returnValue = coursePriceInt[1];
-                break;
+                return COURSE_PRICE_INT[1];
             case "뚝배기 불고기 정식":
-                returnValue = coursePriceInt[2];
-                break;
+                return COURSE_PRICE_INT[2];
             case "전복 갈비탕":
-                returnValue = coursePriceInt[3];
-                break;
+                return COURSE_PRICE_INT[3];
             default:
-                System.out.println("Please type correct menu!");
-                break;
+                throw new IllegalArgumentException("Please type correct menu!");
         }
-        return returnValue;
     }
 
     public int[] getCoursePriceInt() { // 메뉴의 가격을 리스트로 반환
-        return coursePriceInt;
+        return COURSE_PRICE_INT;
     }
 
     public String[] getDrinkMenuList() {
-        return drinkMenu;
+        return DRINK_MENU;
     }
 
     public int getDrinkPrice(String menu) {
-        int returnValue = 0;
         switch (menu) {
             case "탄산음료":
-                returnValue = drinkPriceInt[0];
-                break;
+                return DRINK_PRICE_INT[0];
             case "술":
-                returnValue = drinkPriceInt[1];
-                break;
+                return DRINK_PRICE_INT[1];
             default:
-                System.out.println("Please type correct menu!");
-                break;
+                throw new IllegalArgumentException("Please type correct menu!");
         }
-        return returnValue;
     }
 
     public int[] getDrinkPriceInt() {
-        return drinkPriceInt;
+        return DRINK_PRICE_INT;
     }
 }
