6434fe27c9797c241b6b0e5f01f2c1a351528946
sein12 <moses1008@naver.com>
2024-05-18 10:27:20 +0900

    modify Chef, CouponChecker~~

--- OOP-teamproject/src/Chef.java
+++ OOP-teamproject/src/Chef.java
@@ -2,104 +2,108 @@ import java.util.Scanner;
 import java.util.List;
 
 public class Chef {
-  // 요리 재고 확인 메소드
+    private Scanner scanner = new Scanner(System.in);
+  
+    // 요리 재고 확인 메소드
+    public void getDishStock() { // return 타입을 void로 수정
+        System.out.println("요리 별 재고 확인하겠습니다");
+        for (int i = 0; i < StockAndCost.DishMenu.length; i++) {
+            System.out.println(StockAndCost.DishMenu[i] + "의 재고는 " + StockAndCost.DishStock[i] + "개 남아있습니다");
+        }
+    }
 
-  public int getDishStock() {
-      System.out.println("요리 별 재고 확인하겠습니다");
-      for (int i = 0; i < StockAndCost.DishMenu.size(); i++) {
-          System.out.println(StockAndCost.DishMenu[i]+"의 재고는 "+StockAndCost.DishStock[i]+"개 남아있습니다");    // for문으로 메뉴별 재고 출력
-      }
-  }
+    // 음료 재고 확인 메소드
+    public void getDrinkStock() { // return 타입을 void로 수정
+        System.out.println("음료 별 재고 확인하겠습니다");
+        for (int i = 0; i < StockAndCost.drinkMenu.length; i++) { // 배열로 변경했으므로 .length로 수정
+            System.out.println(StockAndCost.drinkMenu[i] + "의 재고는 " + StockAndCost.drinkStock[i] + "개 남아있습니다");
+        }
+    }
 
-  // 음료 재고 확인 메소드
-  public int getDrinkStock() {
-      System.out.println("음료 별 재고 확인하겠습니다");
-      for (int i = 0; i < StockAndCost.drinkMenu.size(); i++) {
-          System.out.println(StockAndCost.drinkMenu[i]+"의 재고는 "+StockAndCost.drinkStock[i]+"개 남아있습니다");    // for문으로 메뉴별 재고 출력
-      }
-  }
+    public void modifyDishStock() { // return 타입을 void로 수정
+        System.out.println("요리의 재고를 수정하시겠습니까?(추가/감소)를 입력해주세요.: ");
+        String answer = scanner.nextLine();
 
-  // 요리 재고 수정 메소드
-  public int modifyDishStock() {
-      String answer = scanner.nextLine();
-      String count = scanner.nextInt();
-      boolean countUnchanged = True;
-      System.out.println("요리의 재고를 수정하시겠습니까?(추가/감소)를 입력해주세요.: ");
-      answer = scanner.nextLine();
+        if ("추가".equals(answer)) {
+            System.out.println("요리의 재고를 추가하겠습니다.");
+            for (int i = 0; i < StockAndCost.DishMenu.length; i++) {
+                System.out.println("현재 " + StockAndCost.DishMenu[i] + "의 재고는 " + StockAndCost.DishStock[i] + "개 남아있습니다. " + StockAndCost.DishMenu[i] + " 재고를 몇 개 추가할까요?");
+                
+                int count = scanner.nextInt();
+                scanner.nextLine(); // 남아있는 개행 문자 처리
+                
+                RevenueAndCost.expenses += count * StockAndCost.DishCost[i]; // update한 재고 개수의 가격만큼 지출에 update
+                StockAndCost.DishStock[i] += count; // update한 재고 개수만큼 재고에 update
+                
+                System.out.println(StockAndCost.DishMenu[i] + "의 재고가 성공적으로 추가되었습니다. 현재 " + StockAndCost.DishMenu[i] + "의 재고는 " + StockAndCost.DishStock[i] + "개 남아있습니다.");
+            }
+        } else if ("감소".equals(answer)) {
+            System.out.println("요리의 재고를 감소시키겠습니다.");
+            
+            for (int i = 0; i < StockAndCost.DishMenu.length; i++) {
+                boolean countUnchanged = true;
+                
+                while (countUnchanged) { // 값이 바뀌지 않았으면 무한 반복
+                    System.out.println("현재 " + StockAndCost.DishMenu[i] + "의 재고는 " + StockAndCost.DishStock[i] + "개 남아있습니다. " + StockAndCost.DishMenu[i] + " 재고를 몇 개 감소시킬까요?");
+                    int count = scanner.nextInt();
+                    scanner.nextLine(); // 남아있는 개행 문자 처리
+                    
+                    if (count <= StockAndCost.DishStock[i] && count >= 0) { // count가 0 이상이고 재고와 같거나 보다 적을 때
+                        StockAndCost.DishStock[i] -= count; // update한 재고 개수만큼 재고에 update
+                        System.out.println(StockAndCost.DishMenu[i] + "의 재고가 성공적으로 감소되었습니다. 현재 " + StockAndCost.DishMenu[i] + "의 재고는 " + StockAndCost.DishStock[i] + "개 남아있습니다.");
+                        countUnchanged = false; // 값이 바뀌었음 == false
+                    } else if (count > StockAndCost.DishStock[i]) {
+                        System.out.println("현재 재고보다 많이 재고를 감소시킬 수 없습니다. " + StockAndCost.DishMenu[i] + "에 대해 감소시킬 재고의 개수를 올바르게 입력해주세요");
+                    } else if (count < 0) {
+                        System.out.println("음수만큼 재고를 감소시킬 수는 없습니다. " + StockAndCost.DishMenu[i] + "에 대해 감소시킬 재고의 개수를(절대값을) 올바르게 입력해주세요");
+                    }
+                }
+            }
+        }
+    }
+    
+ // 음료 재고 수정 메소드
+    public void modifyDrinkStock() { // return 타입을 void로 수정
+        System.out.println("음료의 재고를 수정하시겠습니까?(추가/감소)를 입력해주세요.: ");
+        String answer = scanner.nextLine();
 
-      if (answer.equals("추가")){
-          System.out.println("요리의 재고를 추가하겠습니다.");
-          for (int i = 0; i < StockAndCost.DishMenu.size(); i++) {
-              System.out.println("현재 "+StockAndCost.DishMenu[i]+"의 재고는 "+StockAndCost.DishStock[i]+"개 남아있습니다. "+ StockAndCost.DishMenu[i]+"재고를 몇개 추가할까요?");    
-              count = scanner.nextInt();    // StockAndCost.DishMenu[i]의 재고를 count만큼 추가
-              FinanceCal.expenses += count*StockAndCost.DishCost[i];    // update한 재고 개수의 가격만큼 지출에 update
-              StockAndCost.DishStock[i] += count;    // update한 재고 개수만큼 재고에 update
-              System.out.println(StockAndCost.DishMenu[i]+"의 재고가 성공적으로 추가되었습니다. 현재"+StockAndCost.DishMenu[i]+"의 재고는 "+StockAndCost.DishStock[i]+"개 남아있습니다.");    
-          }
-      }
-      else if(answer.equals("감소")) {
-          System.out.println("요리의 재고를 감소시키겠습니다.");
-          for (int i = 0; i < StockAndCost.DishMenu.size(); i++) {
-              countUnchanged = true;
-              while(countUnchanged){    // 값이 바뀌지 않았으면 무한 반복
-                  System.out.println("현재 "+StockAndCost.DishMenu[i]+"의 재고는 "+StockAndCost.DishStock[i]+"개 남아있습니다. "+StockAndCost.DishMenu[i]+"재고를 몇개 감소시킬까요?");    
-                  count = scanner.nextInt();    // StockAndCost.courseMenu[i]의 재고를 count만큼 추가
-                  if (count<=StockAndCost.DishStock[i] && count>=0){    // count가 0이상이고 재고와 같거나 보다 적을 때
-                      StockAndCost.DishStock[i] -= count;    // update한 재고 개수만큼 재고에 update
-                      System.out.println(StockAndCost.DishMenu[i]+"의 재고가 성공적으로 감소되었습니다. 현재 "+StockAndCost.DishMenu[i]+"의 재고는 "+StockAndCost.DishStock[i]+"개 남아있습니다.");  
-                      countUnchanged = false;    //값이 바뀌었음 == false
-                  }  
-                  else if (count>StockAndCost.DishStock[i]) {
-                      System.out.println("현재 재고보다 많이 재고를 감소시킬 수 없습니다. "+StockAndCost.DishMenu[i]+"에 대해 감소시킬 재고의 개수를 올바르게 입력해주세요")
-                  }
-                  else if(count<0) {
-                      System.out.println("음수만큼 재고를 감소시킬 수는 없습니다. "+StockAndCost.DishMenu[i]+"에 대해 감소시킬 재고의 개수를(절대값을) 올바르게 입력해주세요")
-                  }
-              }
-
-          }            
-      }
-      // 음료 재고 수정 메소드
-  public int modifyDrinkStock() {
-      String answer = scanner.nextLine();
-      String count = scanner.nextInt();
-      boolean countUnchanged = True;
-      System.out.println("음료의 재고를 수정하시겠습니까?(추가/감소)를 입력해주세요.: ");
-      answer = scanner.nextLine();
-
-      if (answer.equals("추가")){
-          System.out.println("음료의 재고를 추가하겠습니다.");
-          for (int i = 0; i < StockAndCost.drinkMenu.size(); i++) {
-              System.out.println("현재 "+StockAndCost.drinkMenu[i]+"의 재고는 "+StockAndCost.drinkStock[i]+"개 남아있습니다. "+StockAndCost.drinkMenu[i]+"재고를 몇개 추가할까요?");    
-              count = scanner.nextInt();    // StockAndCost.drinkMenu[i]의 재고를 count만큼 추가
-              FinanceCal.expenses += count*StockAndCost.drinkCost[i];    // update한 재고 개수의 가격만큼 지출에 update
-              StockAndCost.drinkStock[i] += count;    // update한 재고 개수만큼 재고에 update
-              System.out.println(StockAndCost.drinkMenu[i]+"의 재고가 성공적으로 추가되었습니다. 현재"+StockAndCost.drinkMenu[i]+"의 재고는 "+StockAndCost.drinkStock[i]+"개 남아있습니다.");    
-          }    // for문 
-      }    // if
-      else if(answer.equals("감소")) {
-          System.out.println("음료의 재고를 감소시키겠습니다.");
-          for (int i = 0; i < StockAndCost.drinkMenu.size(); i++) {
-              countUnchanged = true;
-              while(countUnchanged){    // 값이 바뀌지 않았으면 무한 반복
-                  System.out.println("현재 "+StockAndCost.drinkMenu[i]+"의 재고는 "+StockAndCost.drinkStock[i]+"개 남아있습니다. "+StockAndCost.drinkMenu[i]+"재고를 몇개 감소시킬까요?");    
-                  count = scanner.nextInt();    // StockAndCost.drinkMenu[i]의 재고를 count만큼 추가
-                  if (count<=StockAndCost.drinkStock[i] && count>=0){    // count가 0이상이고 재고와 같거나 보다 적을 때
-                      StockAndCost.drinkStock[i] -= count;    // update한 재고 개수만큼 재고에 update
-                      System.out.println(StockAndCost.drinkMenu[i]+"의 재고가 성공적으로 감소되었습니다. 현재 "+StockAndCost.drinkMenu[i]+"의 재고는 "+StockAndCost.drinkStock[i]+"개 남아있습니다.");  
-                      countUnchanged = false;    // 값이 바뀌었음.
-                  }  
-                  else if (count>StockAndCost.drinkStock[i]) {
-                      System.out.println("현재 재고보다 많이 재고를 감소시킬 수 없습니다. "+StockAndCost.drinkMenu[i]+"에 대해 감소시킬 재고의 개수를 올바르게 입력해주세요")
-                  }
-                  else if(count<0) {
-                      System.out.println("음수만큼 재고를 감소시킬 수는 없습니다. "+StockAndCost.drinkMenu[i]+"에 대해 감소시킬 재고의 개수를(절대값을) 올바르게 입력해주세요")
-                  }
-              }// while닫기
-
-          }   // for문 닫기
-      }    // else if 닫기
-  }    //modifyDrinkStock() 닫기기
+        if ("추가".equals(answer)) {
+            System.out.println("음료의 재고를 추가하겠습니다.");
+            
+            for (int i = 0; i < StockAndCost.drinkMenu.length; i++) { // 배열로 변경했으므로 .length로 수정
+                System.out.println("현재 " + StockAndCost.drinkMenu[i] + "의 재고는 " + StockAndCost.drinkStock[i] + "개 남아있습니다. " + StockAndCost.drinkMenu[i] + " 재고를 몇 개 추가할까요?");
+                int count = scanner.nextInt();
+                scanner.nextLine(); // 남아있는 개행 문자 처리
+                
+                RevenueAndCost.expenses += count * StockAndCost.drinkCost[i]; // update한 재고 개수의 가격만큼 지출에 update
+                StockAndCost.drinkStock[i] += count; // update한 재고 개수만큼 재고에 update
+                
+                System.out.println(StockAndCost.drinkMenu[i] + "의 재고가 성공적으로 추가되었습니다. 현재 " + StockAndCost.drinkMenu[i] + "의 재고는 " + StockAndCost.drinkStock[i] + "개 남아있습니다.");
+            }
+        } else if ("감소".equals(answer)) {
+            System.out.println("음료의 재고를 감소시키겠습니다.");
+            
+            for (int i = 0; i < StockAndCost.drinkMenu.length; i++) { // 배열로 변경했으므로 .length로 수정
+                boolean countUnchanged = true;
+                
+                while (countUnchanged) { // 값이 바뀌지 않았으면 무한 반복
+                    System.out.println("현재 " + StockAndCost.drinkMenu[i] + "의 재고는 " + StockAndCost.drinkStock[i] + "개 남아있습니다. " + StockAndCost.drinkMenu[i] + " 재고를 몇 개 감소시킬까요?");
+                    int count = scanner.nextInt();
+                    scanner.nextLine(); // 남아있는 개행 문자 처리
+                    
+                    if (count <= StockAndCost.drinkStock[i] && count >= 0) { // count가 0 이상이고 재고와 같거나 보다 적을 때
+                        StockAndCost.drinkStock[i] -= count; // update한 재고 개수만큼 재고에 update
+                        System.out.println(StockAndCost.drinkMenu[i] + "의 재고가 성공적으로 감소되었습니다. 현재 " + StockAndCost.drinkMenu[i] + "의 재고는 " + StockAndCost.drinkStock[i] + "개 남아있습니다.");
+                        countUnchanged = false; // 값이 바뀌었음.
+                    } else if (count > StockAndCost.drinkStock[i]) {
+                        System.out.println("현재 재고보다 많이 재고를 감소시킬 수 없습니다. " + StockAndCost.drinkMenu[i] + "에 대해 감소시킬 재고의 개수를 올바르게 입력해주세요");
+                    } else if (count < 0) {
+                        System.out.println("음수만큼 재고를 감소시킬 수는 없습니다. " + StockAndCost.drinkMenu[i] + "에 대해 감소시킬 재고의 개수를(절대값을) 올바르게 입력해주세요");
+                    }
+                }
+            }
+        }
+    }
 
 
 }
--- OOP-teamproject/src/CouponCheckerandDiscount.java
+++ OOP-teamproject/src/CouponCheckerandDiscount.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
+
 public class CouponCheckerandDiscount {//쿠폰 여부 확인 프로토콜
+    Scanner scanner = new Scanner(System.in);
+
+    // 쿠폰 여부 확인 메서드
     public boolean askCoupon() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("쿠폰이 있습니까? (예/아니오): ");
         String response = scanner.nextLine();
         return response.equalsIgnoreCase("예");
     }
-    public void applyDiscount(double originalPrice, boolean hasCoupon) { //hasCoupon = 쿠폰 여부 <- db 참고
-          if (hasCoupon) {
-              double discountPrice = originalPrice * 0.1; // 10% 할인
-              double discountedPrice = originalPrice - discountPrice;
-              System.out.println("10% 할인이 적용되었습니다. 할인된 가격: " + discountedPrice + "원");
-          } else {
-              System.out.println("쿠폰이 없으므로 할인을 적용할 수 없습니다.");
-          }
-      }
+    
+    // 할인 적용 메서드
+    public double applyDiscount(double originalPrice, boolean hasCoupon) { // 반환 타입을 double로 변경
+        if (hasCoupon) {
+            double discountPrice = originalPrice * 0.1; // 10% 할인
+            double discountedPrice = originalPrice - discountPrice;
+            System.out.println("10% 할인이 적용되었습니다. 할인된 가격: " + discountedPrice + "원");
+            return discountedPrice; // 할인된 가격 반환
+        } else {
+            System.out.println("쿠폰이 없으므로 할인을 적용할 수 없습니다.");
+            return originalPrice; // 쿠폰이 없으면 원래 가격 반환
+        }
+    }
 }
